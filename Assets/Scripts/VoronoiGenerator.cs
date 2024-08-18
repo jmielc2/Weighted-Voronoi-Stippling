b600bd69688a78ccce3b010f4808af0b9ad87500
jmielc2 <Jacob.mielczarek@gmail.com>
2024-08-17 21:55:34 -0500

    Voronoi diagram up and running

---
+++ Assets/Scripts/VoronoiGenerator.cs
@@ -0,0 +1,107 @@
+using UnityEngine;
+using Unity.Collections;
+
+// TODO:
+// 1. Draw image to texture.
+// 2. Iterate and make centroidal verenoi diagram.
+
+[RequireComponent(typeof(Camera))]
+public class VoronoiGenerator : MonoBehaviour {
+    [SerializeField]
+    Material material;
+    [SerializeField, Range(1, 1023)]
+    int numPoints;
+    [SerializeField]
+    RenderTexture voronoiTexture;
+
+    Mesh coneMesh;
+    RenderParams renderParams;
+    ComputeBuffer colorBuffer;
+    NativeArray<Vector3> colors;
+    Matrix4x4[] matrices;
+    Camera cam;
+
+    readonly static int colorBufferId = Shader.PropertyToID("_ColorBuffer");
+
+    void OnValidate() {
+        if (colorBuffer != null) {
+            OnDisable();
+            OnEnable();
+        }
+    }
+
+    void OnEnable() {
+        cam = GetComponent<Camera>();
+        GeneratePoints();
+        GenerateConeMesh();
+        material.SetBuffer(colorBufferId, colorBuffer);
+        renderParams = new RenderParams(material);
+        renderParams.worldBounds = new Bounds(Vector3.zero, Vector3.one * 7f);
+    }
+
+    void OnDisable() {
+        colors.Dispose();
+        colorBuffer.Release();
+        colorBuffer = null;
+    }
+
+    void GeneratePoints() {
+        colorBuffer = new ComputeBuffer(numPoints, sizeof(float) * 3);
+        colors = new NativeArray<Vector3>(numPoints, Allocator.Persistent);
+        matrices = new Matrix4x4[numPoints];
+        Vector3 position = Vector3.zero;
+        for (int i = 0; i < numPoints; i++) {
+            position.x = Random.Range(-1f, 1f) * cam.aspect;
+            position.y = Random.Range(-1f, 1f);
+            matrices[i] = Matrix4x4.Translate(position);
+            colors[i] = new Vector3(
+                Random.Range(0f, 1f),
+                Random.Range(0f, 1f),
+                (float)i / (float)numPoints
+            );
+        }
+        colorBuffer.SetData(colors);
+    }
+
+    void GenerateConeMesh() {
+        coneMesh = new Mesh();
+        coneMesh.subMeshCount = 1;
+
+        // Calculate Minimum Number of Cone Slices
+        float radius = Mathf.Sqrt(cam.pixelWidth * cam.pixelWidth + cam.pixelHeight * cam.pixelHeight);
+        float maxAngle = 2f * Mathf.Acos((radius - 1f) / radius);
+        int numSlices = Mathf.CeilToInt((2f * Mathf.PI) / maxAngle);
+
+        // Generate Mesh
+        Vector3[] vertices = new Vector3[numSlices + 1];
+        int[] triangles = new int[numSlices * 3];
+        vertices[0] = Vector3.zero;
+        float angle = 0f;
+        float width = cam.aspect * 2f;
+        radius = Mathf.Sqrt(width * width + 4);
+        for (int i = 1; i < numSlices + 1; i++) {
+            vertices[i] = new Vector3(
+                Mathf.Cos(angle) * radius,
+                Mathf.Sin(angle) * radius,
+                1f
+            );
+            angle -= maxAngle;
+        }
+        for (int i = 0; i < numSlices; i++) {
+            triangles[i * 3] = 0;
+            triangles[(i * 3) + 1] = i;
+            triangles[(i * 3) + 2] = i + 1;
+        }
+        triangles[((numSlices - 1) * 3) + 2] = 1;
+        coneMesh.SetVertices(vertices);
+        coneMesh.SetTriangles(triangles, 0, true, 0);
+    }
+
+    void GenerateVoronoi() {
+        Graphics.RenderMeshInstanced(renderParams, coneMesh, 0, matrices);
+    }
+
+    void Update() {
+        GenerateVoronoi();
+    }
+}
---
+++ Assets/Shaders/PointShader.shader
@@ -0,0 +1,48 @@
+Shader "Custom/Point Shader" {
+    SubShader {
+        Tags { "RenderType"="Opaque" }
+
+        Pass {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            #pragma multi_compile_instancing
+            #pragma instancing_options assumeuniformscaling
+
+            #include "UnityCG.cginc"
+
+            struct appdata {
+                float4 vertex : POSITION;
+                #if defined(INSTANCING_ON)
+                uint instanceID : SV_InstanceID;
+                #endif
+            };
+
+            struct v2f {
+                float4 vertex : SV_POSITION;
+                float3 color : TEXCOORD0;
+            };
+
+            StructuredBuffer<float3> _ColorBuffer;
+
+            v2f vert(appdata v) {
+                #if defined(INSTANCING_ON)
+                UNITY_SETUP_INSTANCE_ID(v);
+                #endif
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                #if defined(INSTANCING_ON)
+                o.color = _ColorBuffer[v.instanceID];
+                #else
+                o.color = float3(0, 0, 0);
+                #endif
+                return o;
+            }
+
+            float4 frag(v2f i) : SV_Target {
+                return float4(i.color, 1);
+            }
+            ENDCG
+        }
+    }
+}
